cf24d2d4102fddf37bc76cb7ef525205ee29c9ac
Bridger Maskrey <maskreybe@live.com>
2015-11-01 22:48:57 -0500

    Made json function-based rather than class based.  Why was I doing that in the first place.

--- Attribute.Common/Data/JsonUtility.cs
+++ Attribute.Common/Data/JsonUtility.cs
@@ -1,23 +1,23 @@
 ﻿using System;
 using System.IO;
-using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 
 namespace Attribute.Common.Data
 {
     /// <summary>
-    /// Base class for JSON-serialized objects.
+    ///     Base class for JSON-serialized objects.
     /// </summary>
-    [DataContract]
-    public abstract class JsonObject
+    public static class JsonUtility
     {
+        #region [-- PUBLIC & PROTECTED METHODS --]
+
         /// <summary>
-        /// Deserialzes a <typeparamref name="TObj"/> from a 
+        ///     Deserialzes a <typeparamref name="TObj" /> from a
         /// </summary>
         /// <typeparam name="TObj"></typeparam>
         /// <param name="inputStream"></param>
         /// <returns></returns>
-        public static TObj DeserializeObject<TObj>(Stream inputStream) where TObj : JsonObject
+        public static TObj DeserializeObject<TObj>(Stream inputStream)
         {
             if (typeof(TObj).IsAbstract)
             {
@@ -29,11 +29,13 @@ namespace Attribute.Common.Data
             return (TObj)serializer.ReadObject(inputStream);
         }
 
-        public static void SerializeObject<TObj>(TObj jsonObject, ref Stream outputStream) where TObj : JsonObject
+        public static void SerializeObject<TObj>(TObj jsonObject, ref Stream outputStream)
         {
             var serializer = new DataContractJsonSerializer(typeof(TObj));
 
             serializer.WriteObject(outputStream, jsonObject);
         }
+
+        #endregion
     }
 }
