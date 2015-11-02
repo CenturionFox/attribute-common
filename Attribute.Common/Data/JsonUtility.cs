71978897ff39bfb6d3fc1dd87ee7b3dd4c810766
Bridger Maskrey <maskreybe@live.com>
2015-11-01 22:50:46 -0500

    Updated documentation.

--- Attribute.Common/Data/JsonUtility.cs
+++ Attribute.Common/Data/JsonUtility.cs
@@ -5,18 +5,19 @@ using System.Runtime.Serialization.Json;
 namespace Attribute.Common.Data
 {
     /// <summary>
-    ///     Base class for JSON-serialized objects.
+    ///     Utility for serializing and deserializing Json data.
     /// </summary>
     public static class JsonUtility
     {
         #region [-- PUBLIC & PROTECTED METHODS --]
 
         /// <summary>
-        ///     Deserialzes a <typeparamref name="TObj" /> from a
+        /// Deserialzes a <typeparamref name="TObj" /> from a
         /// </summary>
-        /// <typeparam name="TObj"></typeparam>
-        /// <param name="inputStream"></param>
+        /// <typeparam name="TObj">The type of the object.</typeparam>
+        /// <param name="inputStream">The input stream.</param>
         /// <returns></returns>
+        /// <exception cref="ArgumentException">Unable to deserialize as an abstract type.</exception>
         public static TObj DeserializeObject<TObj>(Stream inputStream)
         {
             if (typeof(TObj).IsAbstract)
@@ -29,6 +30,12 @@ namespace Attribute.Common.Data
             return (TObj)serializer.ReadObject(inputStream);
         }
 
+        /// <summary>
+        /// Serializes the object.
+        /// </summary>
+        /// <typeparam name="TObj">The type of the object.</typeparam>
+        /// <param name="jsonObject">The json object.</param>
+        /// <param name="outputStream">The output stream.</param>
         public static void SerializeObject<TObj>(TObj jsonObject, ref Stream outputStream)
         {
             var serializer = new DataContractJsonSerializer(typeof(TObj));
